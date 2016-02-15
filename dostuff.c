b1e061c2d01e02457503e7a090181967d5da9352
Loosefish <Loosefish@users.noreply.github.com>
2016-02-15 01:02:46 +0100

    Fully integrated file recursion

--- dostuff.c
+++ dostuff.c
@@ -1,5 +1,7 @@
-// TODO: Magic comments for declaring "local" functions (flag also?)
-// TODO: DOFILE as list of filenames?
+/* TODO
+ * Flag for forced local execution?
+ * DOFILE as list of filenames?
+ */
 #define _GNU_SOURCE
 #include <libgen.h>
 #include <magic.h>
@@ -12,19 +14,55 @@
 
 #include "dostuff.h"
 
-char *EXEC_ARGS_FMT = NULL;
-char *EXEC_FMT = NULL;
-char *FUNC_ARG_SEP = NULL;
-char *FUNC_LOCAL_SUF = NULL;
-char *FUNC_PATTERN = NULL;
-char *NAME_PATTERN = NULL;
+
+char* EXEC_ARGS_FMT = NULL;
+char* EXEC_FMT = NULL;
+char* FUNC_ARG_SEP = NULL;
+char* FUNC_LOCAL_SUF = NULL;
+char* FUNC_PATTERN = NULL;
+char* NAME_PATTERN = NULL;
 
 
+/* Look for dofiles recursively upwards.
+ * Returns: NULL-terminated array of pathnames as char*.
+ */
+char** get_dofiles() {
+	size_t n = 0;
+	char** dofiles = calloc(sizeof(char*), 1);
+
+	char* dir = getcwd(NULL, 0);
+	char* dofile;
+	do {
+		asprintf(&dofile, "%s/%s", dir, get_dofile_name());
+		// file readable?
+		if (access(dofile, R_OK) == 0) {
+			dofiles[n++] = dofile;
+			dofiles = realloc(dofiles, sizeof(char*) * (n + 1));
+		}
+		else {
+			FREE_NULL(dofile);
+		}
+		dir = dirname(dir);
+	}
+	while (strcmp(dir, "/"));
+	FREE_NULL(dir);
+	dofiles[n] = NULL;
+	return dofiles;
+}
+
+
+char* get_dofile_name() {
+	char* name = getenv(DOFILE_ENV);
+	return name ? name : DOFILE_DEFAULT;
+}
+
+
+/* Returns the Dotype for the given path. */
 Dotype get_type(char* path) {
 	const char* magic_type;
 	magic_t magic_cookie = magic_open(MAGIC_MIME);
-	DIE_IF(!magic_cookie, "Can't initialize magic library");
-	DIE_IF(magic_load(magic_cookie, NULL), "Can't load magic database");
+	DIE_IF(!magic_cookie, "Couldn't initialize magic library");
+	DIE_IF(magic_load(magic_cookie, NULL), "Couldn't load magic database");
 
 	magic_type = magic_file(magic_cookie, path);
 	Dotype dotype = UNKNOWN;
@@ -41,6 +79,7 @@ Dotype get_type(char* path) {
 }
 
 
+/* Sets global variables according to the given Dotype. */
 void apply_type(Dotype dotype) {
 	switch (dotype) {
 		case PY:
@@ -64,66 +103,53 @@ void apply_type(Dotype dotype) {
 }
 
 
-char* get_dofile_name() {
-	char* name = getenv(DOFILE_ENV);
-	return name ? name : DOFILE_DEFAULT;
-}
-
-
-char* get_dofile_rec() {
-	char *dir = getcwd(NULL, 0);
-	char *buf = calloc(strlen(dir) + strlen(get_dofile_name()) + 2, 1);
-
-	for (;;) {
-		sprintf(buf, "%s/%s", dir, get_dofile_name());
-		if (access(buf, R_OK) == 0) {
-			FREE_NULL(dir);
-			return buf;
-		}
-		DIE_IF(strcmp(dir, "/") == 0, "Can't locate dofile");
-		dir = dirname(dir);
-	}
-}
-
-
-void print_funcs(char* dofile) {
+/* Prints all function names in the given dofiles. */
+void print_funcs(char** dofiles) {
 	regex_t func_re;
-	char *pattern;
+	char* pattern;
 	size_t nmatch = 2;
 	regmatch_t pmatch[2];
-	asprintf(&pattern, NAME_PATTERN, DOFILE_PREFIX);
-	DIE_IF(!pattern, "Can't allocate memory");
-
-	// compile regex
-	int r = regcomp(&func_re, pattern, REG_NEWLINE);
-	FREE_NULL(pattern);
-	DIE_IF(r, "Can't compile regex.");
-
-	// check lines in dofile against regex
-	FILE* fp = fopen(dofile, "r");
-	DIE_IF(!fp, "Can't open dofile");
-
 	char buffer[1024];
-	while (fgets(buffer, 1024, fp)) {
-		r = regexec(&func_re, buffer, nmatch, pmatch, 0);
-		if (r == 0) {
-			buffer[pmatch[1].rm_eo] = 0;
-			printf("%s\n", buffer + pmatch[1].rm_so);
+
+	char** df = dofiles;
+	while (*df) {
+		// apply settings for current file type
+		apply_type(get_type(*df));
+
+		// prepare regex
+		asprintf(&pattern, NAME_PATTERN, DOFILE_PREFIX);
+		DIE_IF(!pattern, "Couldn't allocate memory");
+		int r = regcomp(&func_re, pattern, REG_NEWLINE);
+		DIE_IF(r, "Couldn't compile regex");
+		FREE_NULL(pattern);
+
+		// check lines in dofile against regex
+		FILE* fp = fopen(*df, "r");
+		DIE_IF(!fp, "Couldn't open dofile");
+
+		while (fgets(buffer, 1024, fp)) {
+			r = regexec(&func_re, buffer, nmatch, pmatch, 0);
+			if (r == 0) {
+				buffer[pmatch[1].rm_eo] = 0;
+				printf("%s\n", buffer + pmatch[1].rm_so);
+			}
 		}
+		fclose(fp);
+		regfree(&func_re);
+		df++;
 	}
-	fclose(fp);
-	regfree(&func_re);
 }
 
 
-void get_func(int argc, char *argv[], char** func, char** args) {
+/* Translates the program args into a function name and an argument string. */
+void get_func(int argc, char* argv[], char** func, char** args) {
 	if (argc > 2) {
 		size_t total = 1;
 		for (size_t i = 2; i < argc; ++i) {
 			total += strlen(argv[i]);
 		}
 		*args = calloc(total + 1 + ((argc - 3) * strlen(FUNC_ARG_SEP)), 1);
-		DIE_IF(!args, "Can't allocate memory");
+		DIE_IF(!args, "Couldn't allocate memory");
 		strcpy(*args, argv[2]);
 		for (size_t i = 3; i < argc; ++i) {
 			strcat(*args, FUNC_ARG_SEP);
@@ -137,42 +163,48 @@ void get_func(int argc, char *argv[], char** func, char** args) {
 	else {
 		asprintf(func, "%s", DOFILE_PREFIX);
 	}
-	DIE_IF(!func, "Can't allocate memory");
+	DIE_IF(!func, "Couldn't allocate memory");
 }
 
 
-void prep_func(char* dofile, char* func) {
+/* Checks if a function exists in a dofile and sets the working directory
+ * according to the function definition.
+ */
+bool prep_func(char* dofile, char* func) {
 	regex_t func_re;
-	char *pattern;
+	char* pattern;
 	asprintf(&pattern, FUNC_PATTERN, func);
-	DIE_IF(!pattern, "Can't allocate memory");
+	DIE_IF(!pattern, "Couldn't allocate memory");
 
 	// compile regex
 	int r = regcomp(&func_re, pattern, REG_NOSUB|REG_NEWLINE);
 	FREE_NULL(pattern);
-	DIE_IF(r, "Can't compile regex.");
+	DIE_IF(r, "Couldn't compile regex.");
 
 	// check lines in dofile against regex
 	FILE* fp = fopen(dofile, "r");
-	DIE_IF(!fp, "Can't open dofile");
+	DIE_IF(!fp, "Couldn't open dofile");
 
-	char buffer[1024];
+	char buffer[1024] = {0};
 	while (fgets(buffer, 1024, fp)) {
 		r = regexec(&func_re, buffer, 0, NULL, 0);
 		if (r == 0) {
-			set_cwd(dofile, buffer);
-			fclose(fp);
-			regfree(&func_re);
-			return;
+			break;
 		}
 	}
-	fprintf(stderr, "Can't locate function '%s'\n", func);
-	exit(EXIT_FAILURE);
+	set_cwd(dofile, buffer);
+	fclose(fp);
+	regfree(&func_re);
+	return r == 0 ? true : false;
 }
 
-
+/* Sets the working directory according to the given function definition.
+ * If the function is to be executed globally, the working directory is set to
+ * the directory containing the dofile.
+ */
 void set_cwd(char* dofile, char* func_line) {
 	size_t suf_n = strlen(FUNC_LOCAL_SUF);
+	// TODO: Use regex? How safe is this?
 	if (strncmp(func_line + strlen(func_line) - 1 - suf_n, FUNC_LOCAL_SUF, suf_n) == 0) {
 		return;
 	}
@@ -181,12 +213,13 @@ void set_cwd(char* dofile, char* func_line) {
 	char* tmp = malloc(n);
 	memcpy(tmp, dofile, n);
 	tmp = dirname(tmp);
-	DIE_IF(chdir(tmp), "Can't set working directory.");
+	DIE_IF(chdir(tmp), "Couldn't set working directory.");
 	FREE_NULL(tmp);
 }
 
 
-void run_func(char* dofile, char* func, char* args) {
+/* Executes a function with or without arguments. */
+void execute_func(char* dofile, char* func, char* args) {
 	char* func_full;
 	if (args) {
 		asprintf(&func_full, EXEC_ARGS_FMT, dofile, func, args);
@@ -194,53 +227,78 @@ void run_func(char* dofile, char* func, char* args) {
 	else {
 		asprintf(&func_full, EXEC_FMT, dofile, func);
 	}
-	DIE_IF(!func_full, "Can't allocate memory");
+	DIE_IF(!func_full, "Couldn't allocate memory");
 	system(func_full);
 	FREE_NULL(func_full);
 }
 
 
-int main(int argc, char *argv[]) {
+/* Tries to execute a function according to the program arguments.
+ * Iterates though dofiles until execution is successful.
+ */
+void execute(int argc, char* argv[], char** dofiles) {
+	char* func = NULL;
+	char* args = NULL;
+	char** df = dofiles;
+	bool done = false;
+	while (*df && !done) {
+		// apply settings for current file type
+		apply_type(get_type(*df));
+
+		// build function string with arguments
+		get_func(argc, argv, &func, &args);
+		// check if function exists and set working dir
+		if (prep_func(*df, func)) {
+			// execute function
+			execute_func(*df, func, args);
+			done = true;
+		}
+		FREE_NULL(func);
+		FREE_NULL(args);
+		df++;
+	}
+}
+
+
+int main(int argc, char* argv[]) {
 	if (argc > 1 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)) {
-		// print function names and exit
 		fprintf(
-			stderr,
-			"Usage: %s [-f | -l | FUNCTION [ARG1 ...]]\n%s\n%s\n%s\n",
-			argv[0],
-			"  -f                   Print path of the local dofile",
-			"  -l                   Print local functions",
-			"  FUNCTION [ARG1 ...]  Call local function with optional arguments"
+		    stderr,
+		    "Usage: %s [-f | -l | FUNCTION [ARG1 ...]]\n%s\n%s\n%s\n",
+		    argv[0],
+		    "  -f                   Print path of the local dofile",
+		    "  -l                   Print local functions",
+		    "  FUNCTION [ARG1 ...]  Call local function with optional arguments"
 		);
 		return EXIT_SUCCESS;
 	}
 
-	// get dofile, determine filetype and apply settings
-	char *dofile = get_dofile_rec();
-	Dotype dotype = get_type(dofile);
-	DIE_IF(dotype == UNKNOWN, "Dofile has unknown type");
-	apply_type(dotype);
+	// gather dofiles
+	char** dofiles = get_dofiles();
+	DIE_IF(!dofiles[0], "Couldn't locate dofile");
 
-	// check arguments
+	// check for flags
 	if (argc > 1 && strcmp(argv[1], "-f") == 0) {
-		// print dofile path and exit
-		printf("%s\n", dofile);
+		// print dofile paths and exit
+		char** df = dofiles;
+		while (*df) {
+			printf("%s\n", *df);
+			df++;
+		}
 	}
 	else if (argc > 1 && strcmp(argv[1], "-l") == 0) {
 		// print function names and exit
-		print_funcs(dofile);
+		print_funcs(dofiles);
 	}
 	else {
-		// build function name and args
-		char *func = NULL;
-		char *args = NULL;
-		get_func(argc, argv, &func, &args);
-
-		// check if function exists, set working dir and run
-		prep_func(dofile, func);
-		run_func(dofile, func, args);
-
-		FREE_NULL(func);
-		FREE_NULL(args);
+		execute(argc, argv, dofiles);
 	}
-	FREE_NULL(dofile);
+
+	// free dofiles
+	char** df = dofiles;
+	while (*df) {
+		FREE_NULL(*df);
+		df++;
+	}
+	FREE_NULL(dofiles);
 }
--- dostuff.h
+++ dostuff.h
@@ -17,12 +17,17 @@
 enum Dotype {UNKNOWN, PY, SH};
 typedef enum Dotype Dotype;
 
-Dotype get_type(char* path);
+
+char** get_dofiles();
 char* get_dofile_name();
-char* get_dofile_rec();
+
+Dotype get_type(char* path);
 void apply_type(Dotype dotype);
-void get_func(int argc, char *argv[], char** func, char** args);
-void prep_func(char* dofile, char* func);
-void print_funcs(char* dofile);
-void run_func(char* dofile, char* func, char* args);
+
+void print_funcs(char** dofiles);
+
+void execute(int argc, char* argv[], char** dofiles);
+void get_func(int argc, char* argv[], char** func, char** args);
+bool prep_func(char* dofile, char* func);
 void set_cwd(char* dofile, char* func_line);
+void execute_func(char* dofile, char* func, char* args);
